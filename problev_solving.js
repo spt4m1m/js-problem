3569651b6c9d450b48257c513bf5940567be1681
spt4m1m <white.hat.tamim@gmail.com>
2023-07-26 11:18:47 +0600

    prblem five  solved

--- problev_solving.js
+++ problev_solving.js
@@ -68,3 +68,23 @@ function twoNumberSum(ary, target) {
 }
 
 console.log(twoNumberSum([1, 3, 6, 8, 11, 15], 9));
+
+
+// Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
+
+function calculator(num1, operator, num2) {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            return num1 / num2;
+        default:
+            return "Invalid operator";
+    }
+}
+
+console.log(calculator(10, "+", 5));

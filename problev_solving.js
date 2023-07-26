88e2f06ab4405fb2e6f701ea5530d009da330c47
spt4m1m <white.hat.tamim@gmail.com>
2023-07-26 11:03:50 +0600

    prblem two solved

--- problev_solving.js
+++ problev_solving.js
@@ -9,3 +9,18 @@ function reverseString(str) {
 }
 
 console.log(reverseString("hello world"));
+
+
+// Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. Example Input: [2, -5, 10, -3, 7] Example Output: 19
+
+function positiveNumberSum(ary) {
+    let sum = 0;
+    for (let num of ary) {
+        if (num > 0) {
+            sum += num;
+        }
+    }
+    return sum;
+}
+
+console.log(positiveNumberSum([2, -5, 10, -3, 7]));

57e18c2a7573622a6fd9fab58e3fbfd76a0d95a8
spt4m1m <white.hat.tamim@gmail.com>
2023-07-26 11:16:09 +0600

    prblem four  solved

--- problev_solving.js
+++ problev_solving.js
@@ -45,3 +45,26 @@ function frequentNumber(arr) {
 }
 
 console.log(frequentNumber([3, 5, 2, 5, 3, 3, 1, 4, 5]));
+
+
+// Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers. Example Input: ([1, 3, 6, 8, 11, 15], 9) Example Output: [1, 2] (numbers at indices 1 and 2: 3 + 6 = 9)
+
+function twoNumberSum(ary, target) {
+    let left = 0;
+    let right = ary.length - 1;
+
+    while (left < right) {
+        const sum = ary[left] + ary[right];
+        if (sum === target) {
+            return [left, right];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return null;
+}
+
+console.log(twoNumberSum([1, 3, 6, 8, 11, 15], 9));
